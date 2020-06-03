49f756fa0ee468e66695622a10b3b4147f8ec47f
Georgi Kukov <gmkukov@gmail.com>
2020-06-03 22:56:16 +0300

    Add ride functionality

--- SnowShare/rides/urls.py
+++ SnowShare/rides/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_car/', views.new_car, name='add_car'),
+    path('add_ride/', views.new_ride, name='add_ride'),
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
