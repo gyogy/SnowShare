349dee9d7adc970ae9c01ed820bd86d96f9cf38d
Georgi Kukov <gmkukov@gmail.com>
2020-06-03 18:56:02 +0300

    register models in admin site

--- SnowShare/rides/admin.py
+++ SnowShare/rides/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Car, Resort, Ride
+
+
 
 # Register your models here.
+
+admin.site.register(Car)
+admin.site.register(Resort)
+admin.site.register(Ride)
