5ac28335fb2c1d1f8e7c51765bea87c98e553544
Niikcety <nikolaineshev1999@gmail.com>
2020-06-03 13:20:15 +0300

    added register, login and logout

--- SnowShare/rides/views.py
+++ SnowShare/rides/views.py
@@ -1,24 +1,46 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
-    return HttpResponse("Hello! You're at the ride sharing index.")
+    return render(request, 'accounts/index.html')
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+
     context = {}
     return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
