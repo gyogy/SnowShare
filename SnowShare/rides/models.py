de4b38a9fd410c1d5a9610beb1ff1c6be69dd8ac
Georgi Kukov <gmkukov@gmail.com>
2020-06-03 18:46:41 +0300

    Switched to django auth user model

--- SnowShare/rides/models.py
+++ SnowShare/rides/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    nickname = models.CharField(max_length=20)
-    first_name = models.CharField(max_length=40)
-    last_name = models.CharField(max_length=40)
-    pw = models.CharField(max_length=40)
+# class User(models.Model):
+#     nickname = models.CharField(max_length=20)
+#     first_name = models.CharField(max_length=40)
+#     last_name = models.CharField(max_length=40)
+#     pw = models.CharField(max_length=40)
 
 
 class Car(models.Model):
@@ -29,9 +30,15 @@ class Car(models.Model):
     capacity = models.IntegerField
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.owner.username}\'s {self.make} {self.typ}'
+
 
 class Resort(models.Model):
     name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
 
 
 class Ride(models.Model):
@@ -39,3 +46,6 @@ class Ride(models.Model):
     driver = models.ForeignKey(User, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'A ride to {self.destination}'
